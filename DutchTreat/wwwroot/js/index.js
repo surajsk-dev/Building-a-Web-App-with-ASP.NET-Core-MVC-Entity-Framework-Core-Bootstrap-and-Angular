086e7248c669697e95f58e72e525884bf9a49c75
dellxps9305 <xps9305@dell.com>
2021-09-05 16:59:51 +0530

    basic - html, css, js, jquery

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,42 @@
+﻿//jquery - alias is - $ symbol
+
+console.log("hello");
+
+//javascript way
+/* var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var Button = document.getElementById("buyButton");
+Button.addEventListener("click", () => {
+    console.log("buying item");
+}); */
+
+
+//jquery way
+
+var theForm = $("#theForm");
+theForm.hide();
+
+var button = $("#buyButton");
+button.on("click", function () {
+    console.log("buying item");
+});
+
+//getting list of items
+var productList = $(".product-props li");
+productList.on("click", function () {
+    console.log("you clicked on" + $(this).text());
+});
+
+//toggel form show-hide
+var $loginToggle = $("#loginToggle");
+var $popupForm = $(".popup-form");
+
+$loginToggle.on("click", function () {
+    //adding time in ()
+    //$popupForm.toggle(1000);
+
+    //adding animation
+    //$popupForm.fadeToggle(1000);
+    $popupForm.slideToggle(1000);
+})
