a3dbd2cc4eb570f66996181ef5f012020d6936e1
dellxps9305 <xps9305@dell.com>
2021-09-06 20:49:52 +0530

    Asp .net MVC demo

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -1,3 +1,4 @@
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,13 +17,37 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IMailService, NullMailService>();
+
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
             app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(cfg => {
+
+                cfg.MapRazorPages();
+
+                cfg.MapControllerRoute(
+                    "Default",
+                    "/{controller}/{action}/{id?}",
+                    new {controller = "App", action = "Index"}
+                    );
+            });
            
         }
     }
