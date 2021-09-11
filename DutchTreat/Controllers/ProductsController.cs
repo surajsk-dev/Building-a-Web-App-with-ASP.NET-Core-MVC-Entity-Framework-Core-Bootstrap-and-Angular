358a7e8cbb513f72491a1b9d435b065a373ea741
dellxps9305 <xps9305@dell.com>
2021-09-11 19:36:39 +0530

    creating api controller

---
+++ DutchTreat/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using DutchTreat.Data;
+using DutchTreat.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProductsController: Controller
+    {
+        private readonly IDutchRepository _repository;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(IDutchRepository repository, ILogger<ProductsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            try
+            {
+                return Ok(_repository.GetAllProducts());
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"failed to get products: {ex}");
+                return BadRequest("failed to get products ");
+            }
+        }
+    }
+}
